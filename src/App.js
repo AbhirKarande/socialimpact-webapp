ca9a26b560f6f92ad5b35dcebbed2a6612305ce2
Abhir Karande <abhir511@gmail.com>
2020-10-28 19:13:50 -0400

    NewsAPI addon

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import axios from "axios";
+import { useState } from 'react';
+var query = "";
+const url = "https://newsapi.org/v2/everything?q="+query+"&apiKey=" + (process.env.REACT_APP_NEWSAPI_TOKEN);
+class App extends React.Component {
+  state = {
+    articles: [],
+    isLoading: true,
+    errors: null
+  };
+  getArticles() {
+    axios
+      .get(
+        url
+      )
+      .then(response =>
+        response.data.articles.map(article => ({
+          date:`${article.publishedAt}`,
+          title: `${article.title}`,
+          url: `${article.url}`
+        }))
+      )
+      .then(articles => {
+        this.setState({
+          articles,
+          isLoading: false
+        });
+      })
+      .catch(error => this.setState({ error, isLoading: false }));
+  }
+  
+  componentDidMount(){
+    this.getArticles();
+  }
+  render() {
+    const { isLoading, articles } = this.state;
+    const [searchTerm, setSearchTerm] = React.useState("");
+    const handleChange = event => {
+      setSearchTerm(event.target.value);
+      setSearchTerm = query;
+    };
+    return (
+      <React.Fragment>
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <h2>#Covid-19</h2>
+        <div>
+          {!isLoading ? (
+            articles.map(article => {
+              const { date, title, url } = article;
+              return (
+                <div key={title}>
+                  <p>{date}</p>
+                  <p>{title}</p>
+                  <p>{url}</p>
+                </div>
+              );
+            })
+          ) : (
+            <p>Loading ...</p>
+          )}
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
